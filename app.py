5507200d5a2ea31ea98f84136462bfd51fe3ce98
trazibo <35712018+trazibo@users.noreply.github.com>
2025-05-04 22:12:13 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder="build", static_url_path="/")
+app = Flask(__name__, static_folder="build", static_url_path="")
 
 @app.route("/")
 def serve():
@@ -12,4 +11,4 @@ def static_proxy(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
